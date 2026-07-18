dda852877be3f0c7e0d0dd9e010c3c4c3f980b0d
Cesar martinez <camartinez578@gmail.com>
2026-07-18 12:32:58 -0600

    Added github link to the mod description

--- mod_info.lua
+++ mod_info.lua
@@ -2,7 +2,7 @@ name = "NextUI.Icons"
 uid = "b36f7a42-7d65-4b9b-a4f3-6b8e3f1c2d90"
 version = 1
 author = "wolfred"
-description = "NextUI strategic icons and upgrade-state overlays."
+description = "NextUI strategic icons and upgrade-state overlays. You can find full mode info at: https://github.com/NextUI-FAF/Icons/"
 
 ui_only = true
 selectable = true
